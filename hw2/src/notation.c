68025cf7fbc6dc1e285aa02e0694523479b69352
RICKY LU <rilu@cs.stonybrook.edu>
2021-02-27 21:13:07 -0500

    Notation have nested comments which are not allowed. THe null around 1583 in notation are replaced by 0 hope it works

--- hw2/src/notation.c
+++ hw2/src/notation.c
@@ -7,6 +7,7 @@
    Nom: notation
    Auteur: Henry Thomas
    Date: 27/11/90
+*/
 /*
 This file is part of NOTATION program.
 
@@ -1298,7 +1299,7 @@ int execute(num,c)
     m->tocol = curcol;
     m->tolig = curlig ;
 
-    /*m->topiece = curpiece ; /* ? */
+    /*m->topiece = curpiece ; ? */
 
     if (configuring)
       (void) configure();
@@ -1481,13 +1482,13 @@ int parse_keyword(token,text)
     free_move_list(m);
     break;
   case TITLE:
-    output_text(dr, T_TITLE, text, NULL);
+    output_text(dr, T_TITLE, text, 0);
     break;
   case SUBTITLE:
-    output_text(dr, T_SUBTITLE, text, NULL);
+    output_text(dr, T_SUBTITLE, text, 0);
     break;
   case SCORE:
-    output_text(dr, T_SCORE, text, NULL);
+    output_text(dr, T_SCORE, text, 0);
     break;
   case LANGUE:
     in_language = find_keyword (t_language, NBLANGUAGES, in_language,
